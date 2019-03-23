9e80442fcd5b5d2d4d5c7967ca06708de0c4186d
RadoslavStanchev <draconissickle@gmail.com>
2019-03-23 12:21:42 +0200

    first angular commit

---
+++ Angular Fundamentals/01.Intro to Angular and TypeScript/index.js
@@ -0,0 +1,9 @@
+var Person = /** @class */ (function () {
+    function Person(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    return Person;
+}());
+var ivan = new Person('Ivan', 20);
+ivan.age = 300;
---
+++ Angular Fundamentals/01.Intro to Angular and TypeScript/index.ts
@@ -0,0 +1,8 @@
+class Person {
+    constructor(public name: string, public age: number) {
+        
+    }
+}
+
+const ivan = new Person('Ivan', 20);
+ivan.age = 300;
